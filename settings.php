b0c767c59fcc566485ac078bcac4384223af1b53
Holger Weischenberg <holger@drupalnoob.de>
2021-02-09 19:08:51 +0100

    initial config files, test content

--- settings.php
+++ settings.php
@@ -297,7 +297,7 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'jGJhPGP7tffiafoYyQJvCELVDOVvw0HDTDOz-M7icq-AWYozFJ0hnZfjQGF5xhwJ9Dy44he-yw';
 
 /**
  * Deployment identifier.
@@ -792,3 +792,9 @@ $databases['default']['default'] = array (
   'driver' => 'sqlite',
 );
 $config_directories['sync'] = '../config';
+$databases['default']['default'] = array (
+  'database' => 'sites/default/files/.ht.sqlite',
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
+  'driver' => 'sqlite',
+);
